7abfd54b052c6da75f30bf3a90e567fab9c1aaba
sergiuiacob1 <sergiu.iacob1@gmail.com>
2019-03-30 12:41:30 +0200

    first neural network to recognize digits, 10% accuracy

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
+import sys
 import pandas as pd
+import numpy as np
 from PIL import Image
+from network import Network
 
 
-def readData():
+def readTrainingData():
     data = pd.read_csv("./data/small_train.csv")
-    return data
+    features = data.values[:, 1:]
+    outputs = data.values[:, 0]
+    return [(feature, output) for feature, output in zip(features, outputs)]
+
+
+# def readTestData():
+#     data = pd.read_csv("./data/test.csv")
+#     features = data.values[:, 1:]
+#     outputs = data.values[:, 0]
+#     return [(feature, output) for feature, output in zip(features, outputs)]
 
 
 def createImage(pixelValues):
@@ -14,5 +26,22 @@ def createImage(pixelValues):
     return img
 
 
-data = readData()
-img = createImage(data.head(1).values[0])
+def normalizeData(data):
+    """``data`` is a list of tuples (features, output)"""
+    features = np.asarray([x[0].astype(float) for x in data])
+    outputs = [x[1] for x in data]
+    maxValues = np.amax(features, axis=1, keepdims=True)
+    features[:, ] /= maxValues
+    return [(x.reshape(len(x), 1), y) for x, y in zip(features, outputs)]
+
+
+def main():
+    data = readTrainingData()
+    data = normalizeData(data)
+    training_data = data[1:len(data) - 10000]
+    validation_data = data[-10000:]
+    net = Network([784, 10, 10])
+    net.SGD(training_data, 1, 10, 3.0, test_data=validation_data)
+
+
+main()
